7f97867cf56330cbbccb03c77a23349064415e60
Rebecca Mahany-Horton <rebeccamahany@gmail.com>
2023-12-28 11:11:48 -0500

    NixOS support for Kolide launcher (#1)

---
+++ modules/kolide-launcher/default.nix
@@ -0,0 +1,55 @@
+flake: { config, lib, pkgs, ... }:
+
+let
+  inherit (lib) types mkEnableOption mkOption mkIf;
+  inherit (flake.packages.x86_64-linux) kolide-launcher;
+  cfg = config.services.kolide-launcher;
+in
+{
+  imports = [];
+
+  options.services.kolide-launcher = {
+    enable = mkEnableOption ''
+      Kolide launcher agent.
+    '';
+  };
+
+  config = mkIf cfg.enable {
+    systemd.services.kolide-launcher = {
+      description = "The Kolide Launcher";
+      after = [ "network.service" "syslog.service" ];
+      wantedBy = [ "multi-user.target" ];
+
+      path = with pkgs; [ patchelf ];
+
+      preStart = ''
+      mkdir -p /var/lib/kolide-k2/k2device-preprod.kolide.com
+
+      if [ ! -d "/etc/kolide-k2" ]; then
+        mkdir -p /etc/kolide-k2
+        echo -n 'secret' > /etc/kolide-k2/secret
+
+        osquerydPath=${flake.packages.x86_64-linux.kolide-launcher}/bin/osqueryd
+        tee /etc/kolide-k2/launcher.flags <<EOF
+with_initial_runner
+autoupdate
+transport jsonrpc
+hostname k2device-preprod.kolide.com
+root_directory /var/lib/kolide-k2/k2device-preprod.kolide.com
+osqueryd_path $osquerydPath
+enroll_secret_path /etc/kolide-k2/secret
+update_channel nightly
+debug
+EOF
+      fi
+'';
+
+      serviceConfig = {
+        Environment = "PATH=/run/wrappers/bin:/bin:/sbin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin";
+        ExecStart = "${flake.packages.x86_64-linux.kolide-launcher}/bin/launcher -config /etc/kolide-k2/launcher.flags";
+        Restart = "on-failure";
+        RestartSec = 3;
+      };
+    };
+  };
+}
