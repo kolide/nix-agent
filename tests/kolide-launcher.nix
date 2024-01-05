6c196ce6d363fe9b0360891771867a702ee94757
Rebecca Mahany-Horton <rebeccamahany@gmail.com>
2024-01-05 13:10:30 -0500

    Set sha for nixpkgs, more test output

--- tests/kolide-launcher.nix
+++ tests/kolide-launcher.nix
@@ -1,7 +1,10 @@
 { flake }:
 
 let
-  nixpkgs = builtins.fetchTarball "https://github.com/nixOS/nixpkgs/archive/23.05.tar.gz";
+  nixpkgs = builtins.fetchTarball {
+    url = "https://github.com/nixOS/nixpkgs/archive/23.05.tar.gz";
+    sha256 = "sha256:10wn0l08j9lgqcw8177nh2ljrnxdrpri7bp0g7nvrsn9rkawvlbf"; 
+  };
   pkgs = import nixpkgs { config = {}; overlays = []; };
 in
 
