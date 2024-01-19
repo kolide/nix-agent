03008cf90ad8842a677e1e411da925fea2b8c69d
Rebecca Mahany-Horton <rebeccamahany@gmail.com>
2024-01-19 13:21:27 -0500

    Adjust check

--- tests/kolide-launcher.nix
+++ tests/kolide-launcher.nix
@@ -51,45 +51,43 @@ pkgs.nixosTest {
       ci = builtins.getEnv "CI";
     in
     ''
-      if not "${ci}":
-        return
+      if "${ci}":
+        machine.start()
 
-      machine.start()
+        with subtest("log in to MATE"):
+          machine.wait_for_unit("display-manager.service", timeout=120)
+          machine.wait_for_file("${xauthority}")
+          machine.succeed("xauth merge ${xauthority}")
+          machine.wait_until_succeeds("pgrep marco")
+          machine.wait_for_window("marco")
+          machine.wait_until_succeeds("pgrep mate-panel")
+          machine.wait_for_window("Top Panel")
+          machine.wait_for_window("Bottom Panel")
+          machine.wait_until_succeeds("pgrep caja")
+          machine.wait_for_window("Caja")
+          machine.sleep(20)
+          machine.screenshot("test-screen1.png")
 
-      with subtest("log in to MATE"):
-        machine.wait_for_unit("display-manager.service", timeout=120)
-        machine.wait_for_file("${xauthority}")
-        machine.succeed("xauth merge ${xauthority}")
-        machine.wait_until_succeeds("pgrep marco")
-        machine.wait_for_window("marco")
-        machine.wait_until_succeeds("pgrep mate-panel")
-        machine.wait_for_window("Top Panel")
-        machine.wait_for_window("Bottom Panel")
-        machine.wait_until_succeeds("pgrep caja")
-        machine.wait_for_window("Caja")
-        machine.sleep(20)
-        machine.screenshot("test-screen1.png")
+        with subtest("set up secret file"):
+          machine.copy_from_host("${./test-secret}", "/etc/kolide-k2/secret")
 
-      with subtest("set up secret file"):
-        machine.copy_from_host("${./test-secret}", "/etc/kolide-k2/secret")
+        with subtest("launcher service runs and is set up correctly"):
+          machine.systemctl("stop kolide-launcher.service")
+          machine.systemctl("start kolide-launcher.service")
+          machine.wait_for_unit("kolide-launcher.service", timeout=120)
+          machine.wait_for_file("/var/kolide-k2/k2device.kolide.com/debug.json")
+          machine.sleep(60)
+          machine.screenshot("test-screen2.png")
 
-      with subtest("launcher service runs and is set up correctly"):
-        machine.systemctl("stop kolide-launcher.service")
-        machine.systemctl("start kolide-launcher.service")
-        machine.wait_for_unit("kolide-launcher.service", timeout=120)
-        machine.wait_for_file("/var/kolide-k2/k2device.kolide.com/debug.json")
-        machine.sleep(60)
-        machine.screenshot("test-screen2.png")
+        with subtest("osquery runs"):
+          machine.wait_until_succeeds("pgrep osqueryd", timeout=30)
+          machine.screenshot("test-screen3.png")
 
-      with subtest("osquery runs"):
-        machine.wait_until_succeeds("pgrep osqueryd", timeout=30)
-        machine.screenshot("test-screen3.png")
+        with subtest("launcher desktop runs (test incomplete for now)"):
+          machine.wait_for_file("/var/kolide-k2/k2device.kolide.com/kolide.png")
+          machine.wait_for_file("/var/kolide-k2/k2device.kolide.com/menu.json")
+          machine.screenshot("test-screen4.png")
 
-      with subtest("launcher desktop runs (test incomplete for now)"):
-        machine.wait_for_file("/var/kolide-k2/k2device.kolide.com/kolide.png")
-        machine.wait_for_file("/var/kolide-k2/k2device.kolide.com/menu.json")
-        machine.screenshot("test-screen4.png")
-
-      machine.shutdown()
+        machine.shutdown()
     '';
 }
