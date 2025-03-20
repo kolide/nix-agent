0a80b35f15fba54b29048a722dc4589a90b18ec2
RebeccaMahany <rebeccamahany@gmail.com>
2025-03-20 13:42:08 -0400

    Add launcher arm64 sha

--- kolide-launcher.nix
+++ kolide-launcher.nix
@@ -24,7 +24,7 @@ let
       aarch64-linux = {
         src = fetchzip {
           url = "https://dl.kolide.co/kolide/launcher/linux/arm64/launcher-1.12.3.tar.gz";
-          sha256 = "";
+          sha256 = "sha256-shJMPtz3txgRRw0sc9bNgYUy9cw9TkVGYI/nAO6TbDg=";
           name = "launcher";
         };
         osqSrc = fetchzip {
