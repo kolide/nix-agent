d397a51cf85b3a44180ae5c3ad0fb8a6f6056d14
RebeccaMahany <rebeccamahany@gmail.com>
2025-03-20 15:40:03 -0400

    Update arm build to version that is actually arm

--- kolide-launcher.nix
+++ kolide-launcher.nix
@@ -23,7 +23,7 @@ let
 
       aarch64-linux = {
         src = fetchzip {
-          url = "https://dl.kolide.co/kolide/launcher/linux/arm64/launcher-1.12.3.tar.gz";
+          url = "https://dl.kolide.co/kolide/launcher/linux/arm64/launcher-1.17.0-8-g5039a8f7.tar.gz";
           sha256 = "sha256-shJMPtz3txgRRw0sc9bNgYUy9cw9TkVGYI/nAO6TbDg=";
           name = "launcher";
         };
