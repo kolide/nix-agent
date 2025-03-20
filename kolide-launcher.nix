c1a7c82edfed7fd27d307bcb3039b4afd71f2f8d
RebeccaMahany <rebeccamahany@gmail.com>
2025-03-20 13:32:53 -0400

    Try doing it completely differently

--- kolide-launcher.nix
+++ kolide-launcher.nix
@@ -5,18 +5,32 @@
 }:
 
 let
-  dist =
+  sources =
     {
       x86_64-linux = {
-        arch = "amd64";
-        launchersha256 = "sha256-shJMPtz3txgRRw0sc9bNgYUy9cw9TkVGYI/nAO6TbDg=";
-        osquerysha256 = "sha256-rcmoXshD9pwaTN2dgJm9ZmuCuNQkPgLxghtMGYfhfYE=";
+        src = fetchzip {
+          url = "https://dl.kolide.co/kolide/launcher/linux/amd64/launcher-1.12.3.tar.gz";
+          sha256 = "sha256-shJMPtz3txgRRw0sc9bNgYUy9cw9TkVGYI/nAO6TbDg=";
+          name = "launcher";
+        };
+        osqSrc = fetchzip {
+          url = "https://dl.kolide.co/kolide/osqueryd/linux/amd64/osqueryd-5.14.1.tar.gz";
+          sha256 = "sha256-rcmoXshD9pwaTN2dgJm9ZmuCuNQkPgLxghtMGYfhfYE=";
+          name = "osqueryd";
+        };
       };
 
       aarch64-linux = {
-        arch = "arm64";
-        launchersha256 = "sha256-AAAAAAAA";
-        osquerysha256 = "sha256-AAAAAAAA";
+        src = fetchzip {
+          url = "https://dl.kolide.co/kolide/launcher/linux/arm64/launcher-1.12.3.tar.gz";
+          sha256 = "";
+          name = "launcher";
+        };
+        osqSrc = fetchzip {
+          url = "https://dl.kolide.co/kolide/osqueryd/linux/amd64/osqueryd-5.14.1.tar.gz";
+          sha256 = "";
+          name = "osqueryd";
+        };
       };
     };
 in
@@ -25,18 +39,8 @@ stdenv.mkDerivation rec {
   pname = "kolide-launcher";
   version = "1.12.3";
 
-  src = fetchzip {
-    url = "https://dl.kolide.co/kolide/launcher/linux/${dist.arch}/launcher-${version}.tar.gz";
-    sha256 = "${dist.launchersha256}";
-    name = "launcher";
-  };
-
-  osqSrc = fetchzip {
-    inherit (dist) osquerysha256;
-    url = "https://dl.kolide.co/kolide/osqueryd/linux/${dist.arch}/osqueryd-5.14.1.tar.gz";
-    sha256 = "${dist.osquerysha256}";
-    name = "osqueryd";
-  };
+  src = sources.${system}.src;
+  osqSrc = sources.${system}.osqSrc;
 
   nativeBuildInputs = [
     autoPatchelfHook
