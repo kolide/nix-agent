251f6c80ca81c458640e9dffb1be2e47671a3b4b
RebeccaMahany <rebeccamahany@gmail.com>
2025-03-20 13:11:18 -0400

    First pass at arm64 support

--- kolide-launcher.nix
+++ kolide-launcher.nix
@@ -4,19 +4,36 @@
 , stdenv
 }:
 
+let
+  dist =
+    {
+      x86_64-linux = {
+        arch = "amd64";
+        launchersha256 = "sha256-shJMPtz3txgRRw0sc9bNgYUy9cw9TkVGYI/nAO6TbDg=";
+        osquerysha256 = "sha256-rcmoXshD9pwaTN2dgJm9ZmuCuNQkPgLxghtMGYfhfYE=";
+      };
+
+      aarch64-linux = {
+        arch = "arm64";
+        launchersha256 = "";
+        osquerysha256 = "";
+      };
+    }
+in
+
 stdenv.mkDerivation rec {
   pname = "kolide-launcher";
   version = "1.12.3";
 
   src = fetchzip {
-    url = "https://dl.kolide.co/kolide/launcher/linux/amd64/launcher-${version}.tar.gz";
-    sha256 = "sha256-shJMPtz3txgRRw0sc9bNgYUy9cw9TkVGYI/nAO6TbDg=";
+    url = "https://dl.kolide.co/kolide/launcher/linux/${dist.arch}/launcher-${version}.tar.gz";
+    sha256 = "${dist.launchersha256}";
     name = "launcher";
   };
 
   osqSrc = fetchzip {
-    url = "https://dl.kolide.co/kolide/osqueryd/linux/amd64/osqueryd-5.14.1.tar.gz";
-    sha256 = "sha256-rcmoXshD9pwaTN2dgJm9ZmuCuNQkPgLxghtMGYfhfYE=";
+    url = "https://dl.kolide.co/kolide/osqueryd/linux/${dist.arch}/osqueryd-5.14.1.tar.gz";
+    sha256 = "${dist.osquerysha256}";
     name = "osqueryd";
   };
 
@@ -35,7 +52,7 @@ stdenv.mkDerivation rec {
   meta = with lib; {
     homepage = "https://www.kolide.com";
     description = "Kolide Endpoint Agent";
-    platforms = [ "x86_64-linux" ];
+    platforms = [ "x86_64-linux", "aarch64-linux" ];
     license = {
       fullName = "The Kolide Enterprise Edition (EE) license";
       url = "https://github.com/kolide/launcher/blob/main/LICENSE";
